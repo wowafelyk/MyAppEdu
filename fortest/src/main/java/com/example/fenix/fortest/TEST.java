3a18fefd8d2e609041777393cb924d7d0f375e72
Volodymyr Felyk <wowa.feluk@gmail.com>
2015-05-27 02:48:05 +0300

    - fortest (XML)
    Create Layout from *.xml file.

--- fortest/src/main/java/com/example/fenix/fortest/TEST.java
+++ fortest/src/main/java/com/example/fenix/fortest/TEST.java
@@ -12,58 +12,9 @@ public class TEST extends Activity {
     private LinearLayout root;
 
     @Override
-    public void onCreate(Bundle state) {
+     public void onCreate(Bundle state) {
         super.onCreate(state);
-        LinearLayout.LayoutParams containerParams
-                = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                0.0F);
-        LinearLayout.LayoutParams widgetParams
-                = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                1.0F);
-        root = new LinearLayout(this);
-        root.setOrientation(LinearLayout.VERTICAL);
-        root.setBackgroundColor(Color.LTGRAY);
-        root.setLayoutParams(containerParams);
-
-        LinearLayout ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.HORIZONTAL);
-        ll.setBackgroundColor(Color.GRAY);
-        ll.setLayoutParams(containerParams);
-        root.addView(ll);
-
-        EditText tb = new EditText(this);
-        tb.setText(R.string.defaultLeftText);
-        tb.setFocusable(false);
-        tb.setLayoutParams(widgetParams);
-        ll.addView(tb);
-
-        tb = new EditText(this);
-        tb.setText(R.string.defaultRightText);
-        tb.setFocusable(false);
-        tb.setLayoutParams(widgetParams);
-        ll.addView(tb);
-
-        ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.HORIZONTAL);
-        ll.setBackgroundColor(Color.DKGRAY);
-        ll.setLayoutParams(containerParams);
-        root.addView(ll);
-
-        Button b = new Button(this);
-        b.setText(R.string.labelRed);
-        b.setTextColor(Color.RED);
-        b.setLayoutParams(widgetParams);
-        ll.addView(b);
-
-        b = new Button(this);
-        b.setText(R.string.labelGreen);
-        b.setTextColor(Color.GREEN);
-        b.setLayoutParams(widgetParams);
-        ll.addView(b);
-        setContentView(root);
+        setContentView(R.layout.activity_test);
+        root = (LinearLayout) findViewById(R.id.root);
     }
 }
