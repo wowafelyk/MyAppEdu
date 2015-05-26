df1c54c42c402c20a4c63c2af7af537ad5f2dbbf
Volodymyr Felyk <wowa.feluk@gmail.com>
2015-05-27 01:04:28 +0300

    - fortest
    Create Layout from *.java file using comands.

--- fortest/src/main/java/com/example/fenix/fortest/TEST.java
+++ fortest/src/main/java/com/example/fenix/fortest/TEST.java
@@ -1,38 +1,69 @@
 package com.example.fenix.fortest;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-
-public class TEST extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
-    }
+public class TEST extends Activity {
+    private LinearLayout root;
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_test, menu);
-        return true;
-    }
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        LinearLayout.LayoutParams containerParams
+                = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                0.0F);
+        LinearLayout.LayoutParams widgetParams
+                = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                1.0F);
+        root = new LinearLayout(this);
+        root.setOrientation(LinearLayout.VERTICAL);
+        root.setBackgroundColor(Color.LTGRAY);
+        root.setLayoutParams(containerParams);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.HORIZONTAL);
+        ll.setBackgroundColor(Color.GRAY);
+        ll.setLayoutParams(containerParams);
+        root.addView(ll);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        EditText tb = new EditText(this);
+        tb.setText(R.string.defaultLeftText);
+        tb.setFocusable(false);
+        tb.setLayoutParams(widgetParams);
+        ll.addView(tb);
 
-        return super.onOptionsItemSelected(item);
+        tb = new EditText(this);
+        tb.setText(R.string.defaultRightText);
+        tb.setFocusable(false);
+        tb.setLayoutParams(widgetParams);
+        ll.addView(tb);
+
+        ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.HORIZONTAL);
+        ll.setBackgroundColor(Color.DKGRAY);
+        ll.setLayoutParams(containerParams);
+        root.addView(ll);
+
+        Button b = new Button(this);
+        b.setText(R.string.labelRed);
+        b.setTextColor(Color.RED);
+        b.setLayoutParams(widgetParams);
+        ll.addView(b);
+
+        b = new Button(this);
+        b.setText(R.string.labelGreen);
+        b.setTextColor(Color.GREEN);
+        b.setLayoutParams(widgetParams);
+        ll.addView(b);
+        setContentView(root);
     }
 }
